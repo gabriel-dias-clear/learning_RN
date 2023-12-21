2468c1626972c6b41ebbff7948d865d5b790df28
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-12-21 12:36:40 -0300

    Refactoring Imports and Cleaning Comments

---
+++ exercicios/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+
+import Mega from './components/mega/Mega'
+
+export default () => (
+    <SafeAreaView style={style.App}>
+        <Mega qtdeNumeros={12} />
+    </SafeAreaView>
+)
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+    }
+})
